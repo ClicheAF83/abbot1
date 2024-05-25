91de4cfc1df052662465b5abc089ea88999f6734
Isaac Odula <isaomo360@gmail.com>
2024-05-25 16:20:05 +0300

    admin page saved and a posts database created deleting the dummy data

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts = [
-    {
-        'author' : 'CLicheAF',
-        'title' : 'monkey',
-        'content' : 'content 1, is a lot. Check me out',
-        'date_posted' : 'August 27, 2003'
-    },
-    {
-        'author' : 'molybdenum',
-        'title' : 'cat',
-        'content' : 'Sleeping a lot. someone needs to wake me up',
-        'date_posted' : 'August 27, 2003'
-
-    }
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts' : posts
+        'posts' : Post.objects.all()
     }
     return render(request,'blog/home.html', context)
 
