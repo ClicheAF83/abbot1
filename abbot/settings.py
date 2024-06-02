538cf9b8e0452634adc41e126a8814cd214a7907
Isaac Odula <isaomo360@gmail.com>
2024-06-02 17:39:43 +0300

    profile pictures added and default profile created with each signup

--- abbot/settings.py
+++ abbot/settings.py
@@ -120,9 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_ROOT = BASE_DIR.joinpath('media')
+MEDIA_URL = '/media/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
